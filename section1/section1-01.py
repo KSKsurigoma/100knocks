1af188b9ccc29df0ce37b926f835a2672927099b
KSKsurigoma <u775032i@ecs.osaka-u.ac.jp>
2019-08-22 11:11:50 +0900

    solve Japanese bytes problems

--- section1/section1-01.py
+++ section1/section1-01.py
@@ -3,6 +3,6 @@ s1 = 'パタトクカシーー'
 s2 = ""
 for i in range(1, 8):
     if(i % 2 == 1):
-        n = 3*i-2
-        s2 += s1[n-1] + s1[n] + s1[n+1]
+        s2 += s1[i]
 print(s2)
+print(len(s1))
--- section1/section1-02.py
+++ section1/section1-02.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-def jp(s, n):
-    return s[n-1] + s[n] + s[n+1]
-
-
 s1 = "パトカー"
 s2 = "タクシー"
 s3 = ""
-for i in range(1, 5):
-    n = 3*i-2
-    s3 += jp(s1, n) + jp(s2, n)
+for i in range(4):
+    s3 += s1[i] + s2[i]
 print(s3)
