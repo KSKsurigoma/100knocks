ee402fe5eca4d6b47812f249083ef5a443264b8b
KSKsurigoma <u775032i@ecs.osaka-u.ac.jp>
2019-08-22 18:13:29 +0900

    modify ngram

--- section1/section1_06.py
+++ section1/section1_06.py
@@ -3,22 +3,22 @@ import importlib
 
 
 def gram_search(s, ngram, name):
-    for i in range(len(ngram)):
-        if s == ngram[i]:
-            print('\'{0}\'は{1}に含まれる'.format(s, name))
-            break
+    if s in ngram:
+        print("'{0}'は{1}に含まれる".format(s, name))
     else:
-        print('\'{0}\'は{1}に含まれない'.format(s, name))
+        print("'{0}'は{1}に含まれない".format(s, name))
 
 
 s1 = "paraparaparadise"
 s2 = "paragraph"
-ngram_module = importlib.import_module('section1_05')
+ngram_module = importlib.import_module("section1_05")
 X = set(ngram_module.char_ngram(s1, 2))
 Y = set(ngram_module.char_ngram(s2, 2))
+union = X | Y
 intersection = X & Y
 difference = X - Y
-print('XとYの和集合 : {}'.format(intersection))
-print('XとYの差集合 : {}'.format(difference))
-gram_search('se', list(X), 'X')
-gram_search('se', list(Y), 'Y')
+print("XとYの和集合 : {}".format(union))
+print("XとYの差集合 : {}".format(difference))
+print("XとYの積集合 : {}".format(intersection))
+gram_search("se", list(X), "X")
+gram_search("se", list(Y), "Y")
