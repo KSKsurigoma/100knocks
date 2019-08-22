395f599c94aa250527be784b122fc743ada58fa7
KSKsurigoma <u775032i@ecs.osaka-u.ac.jp>
2019-08-22 17:01:17 +0900

    add init

--- section2/section2_10.py
+++ section2/section2_10.py
@@ -1,6 +1,11 @@
-file_r = 'hightemp.txt'
-count = 0
-with open(file_r) as f_r:
-    for line in f_r:
-        count += 1
-print(count)
+def line_count():
+    file_r = 'hightemp.txt'
+    count = 0
+    with open(file_r) as f_r:
+        for _ in f_r:
+            count += 1
+    return count
+
+
+if __name__ == "__main__":
+    print(line_count())
