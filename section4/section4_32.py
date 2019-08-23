97a7c75b9a62736808fded1b10837175df2e1364
KSKsurigoma <u775032i@ecs.osaka-u.ac.jp>
2019-08-23 16:55:51 +0900

    base extraction

--- section4/section4_32.py
+++ section4/section4_32.py
@@ -1,10 +1,10 @@
 import section4_30 as sec30
 
 file_r = "neko.txt.mecab"
-file_w = "output31"
+file_w = "output32"
 sentence_list = sec30.sentence_mapping_list(file_r)
 with open(file_w, mode="w") as f_w:
     for line in sentence_list:
         for mapping in line:
             if mapping['pos'] == '動詞':
-                f_w.write(str(mapping['surface'])+"\n")
+                f_w.write(str(mapping['base'])+"\n")
