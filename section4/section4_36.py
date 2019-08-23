89a95686f92d076e73035bf45554d79c3176b8ae
KSKsurigoma <u775032i@ecs.osaka-u.ac.jp>
2019-08-23 19:14:57 +0900

    plot halfway

--- section4/section4_36.py
+++ section4/section4_36.py
@@ -1,16 +1,25 @@
 import collections
 import section4_30 as sec30
 
-file_r = "neko.txt.mecab"
-file_w = "output36"
-sentence_list = sec30.sentence_mapping_list(file_r)
-noun_list = []
-with open(file_w, mode="w") as f_w:
-    for line in sentence_list:
-        for mapping in line:
-            noun_list.append(mapping["surface"])
-    noun_counter = collections.Counter(noun_list)
-    noun_list_sorted = noun_counter.most_common()
-    for noun in noun_list_sorted:
-        output = "{0} : {1}".format(noun[0], noun[1])
-        f_w.write(output + "\n")
+
+def word_appearance_frequency(file_r: str):
+    file_w = "output36"
+    sentence_list = sec30.sentence_mapping_list(file_r)
+    noun_list = []
+    frequency_list = []
+    with open(file_w, mode="w") as f_w:
+        for line in sentence_list:
+            for mapping in line:
+                noun_list.append(mapping["surface"])
+        noun_counter = collections.Counter(noun_list)
+        noun_list_sorted = noun_counter.most_common()
+        for noun in noun_list_sorted:
+            output = "{0} : {1}".format(noun[0], noun[1])
+            f_w.write(output + "\n")
+            frequency_list.append(noun)
+    return frequency_list
+
+
+if __name__ == "__main__":
+    file_r = "neko.txt.mecab"
+    word_appearance_frequency(file_r)
---
+++ section4/section4_37.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import section4_36 as sec36
+
+file_r = "neko.txt.mecab"
+file_w = "output37"
+length = 10
+frequency_list = sec36.word_appearance_frequency(file_r)
+word = []
+freq = []
+with open(file_w, mode="w") as f_w:
+    for i in range(10):
+        f_w.write(str(frequency_list[i]) + "\n")
+        word.append(frequency_list[i][0])
+        freq.append(frequency_list[i][1])
+    # for line in sentence_list:
+    #     for mapping in line:
+    #         noun_list.append(mapping["surface"])
+    # noun_counter = collections.Counter(noun_list)
+    # noun_list_sorted = noun_counter.most_common()
+    # for noun in noun_list_sorted:
+    #     output = "{0} : {1}".format(noun[0], noun[1])
+    #     f_w.write(output + "\n")
+    #     noun_list.append(output)
+
+plt.bar(word, freq)
+plt.show()
